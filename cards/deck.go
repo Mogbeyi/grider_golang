08739cf3cbe2e896fc2b98536abca64c6624abf4
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-06-08 13:20:54 +0100

    Add functionalities to: 1 Create a new deck 2 Print a new deck

---
+++ cards/deck.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type (
+	deck []string
+	suit []string
+	value []string
+)
+
+func newDeck() deck {
+	cards := deck{}
+	suits := suit{"Spades", "Diamonds", "Hearts", "Clubs"}
+	values := value{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range suits {
+		for _, value := range values {
+			cards = append(cards, value + " of " + suit)
+		} 
+	}
+
+	return cards
+
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
