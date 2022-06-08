739bdb85bacd01f6950e57e6853c34221281c342
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-06-08 14:07:59 +0100

    Implement the deal function

--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck() 
-	cards.print()	
+	// cards.print()	
+
+	hand, remainingDeck := deal(cards, 7)
+	hand.print()
+	fmt.Println()
+	remainingDeck.print()
 }
