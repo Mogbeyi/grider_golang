fe6c0e0cf478ac73920e129ffcee3a8cf8d86947
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-06-09 02:00:22 +0100

    Write test to check first and last elements of a deck

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -10,4 +10,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck lenght of 16 but go %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be ace of spades but got %v", d[0])
+	}
+
+	if d[len(d) - 1] != "Four of Clubs" {
+		t.Errorf("Expected last card to be four of clubs but got %v", d[len(d) - 1])
+	}
 } 
