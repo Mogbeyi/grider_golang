e99d79320475989c54b841e3c4faf100b6e01cc0
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-06-09 19:14:52 +0100

    Add contact info to person

--- struct/main.go
+++ struct/main.go
@@ -1,13 +1,27 @@
 package main
 
 import "fmt"
-
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
+}
+type contactInfo struct {
+	email string
+	zipcode int
 }
 
 func main() {
 	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
+	jim := person {
+		firstName: "Jim",
+		lastName: "Daniel",
+		contact: contactInfo {
+			email: "jim@gmail.com",
+			zipcode: 20090,
+		},
+	}
+
+	fmt.Printf("%+v", alex)
+	fmt.Printf("%+v", jim)
 }
